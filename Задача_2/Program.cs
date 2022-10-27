8b0c0243920a791f5de25981f57f4b2983bd9d4c
Кушнаренко Григорий <Gr4n777.yandex.ru>
2022-10-27 05:54:46 +0300

    Домашка 7

---
+++ Задача_2/Program.cs
@@ -0,0 +1,61 @@
+﻿/*Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.*/
+
+Console.Write("Введите позицию элемент: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите позицию элемента: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите количество строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int col = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int rows, int column, int minValue, int maxValue)
+{
+    int[,] matrix = new int[rows, column];
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+int FindElement(int[,] MyMatrix)
+{
+    int element = 0;
+    for (int i = 0; i < MyMatrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < MyMatrix.GetLength(1); j++)
+            {
+                if (i == m - 1 && j == n - 1) 
+                element = MyMatrix[i, j];
+            }
+    }
+    return element;
+}
+
+int[,] myArray = GetArray(row, col, 0, 10);
+PrintArray(myArray);
+Console.WriteLine();
+
+if (FindElement(myArray) > 0)
+    {
+    Console.WriteLine("Значение выбранного элемента: " + FindElement(myArray));
+    }
+else
+    {
+    Console.WriteLine("Элемента с указанными позициями нет в этом массиве");
+    }
---
+++ Задача_3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Write("Введите количество строк:  ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов:  ");
+int colums = Convert.ToInt32(Console.ReadLine());
+double[,] table = new double[rows, colums];
+
+void PrintArray(double[,] tab) 
+{
+    for (int rows = 0; rows < tab.GetLength(0); rows++) 
+    {
+        for (int colums = 0; colums < tab.GetLength(1); colums++) 
+        {
+            Console.Write($"{tab[rows, colums],4} "); 
+        }
+        Console.WriteLine(); 
+    }
+}
+
+void FillArray(double[,] tab) 
+{
+    for (int rows = 0; rows < tab.GetLength(0); rows++)
+    {
+        for (int colums = 0; colums < tab.GetLength(1); colums++)
+        {
+            tab[rows, colums] = Convert.ToDouble(new Random().Next(1, 10)); 
+        }
+    }
+}
+
+void PrintArrayAvg(double[,] tab)
+{
+    for (int j = 0; j < table.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < table.GetLength(0); i++)
+        {
+            sum += Convert.ToDouble(table[i, j]);
+        }
+        Console.Write($"{sum / table.GetLength(0),4} "); 
+    }
+    Console.ReadLine();
+}
+
+FillArray(table);
+Console.WriteLine(); 
+PrintArray(table); 
+Console.WriteLine(); 
+PrintArrayAvg(table); 
